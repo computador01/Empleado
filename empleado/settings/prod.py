533a024f039523d2834a8b3babdf4ae9a63b5b57
computador01 <j.2204@hotmail.com>
2022-01-16 22:01:13 -0500

    git actualizacion

---
+++ empleado/settings/prod.py
@@ -0,0 +1,25 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbempleado',
+        'USER': 'wilson',
+        'PASSWORD': 'computador01',
+        'HOST': 'localhost',
+        'NAME': 'dbempleado',
+        'PORT': '5432',
+    }
+}
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
